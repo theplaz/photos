0ce9d5f3a6ab886cf717003f7af4908ea9c709b1
Michael Plasmeier <2670107+theplaz@users.noreply.github.com>
2020-05-03 12:45:59 -0700

    Update OrgPhotos.ps1
    get exif tools working

--- OrgPhotos.ps1
+++ OrgPhotos.ps1
@@ -1,69 +1,74 @@
-function Start-Proc  {
-     param (
-             [string]$exe = $(Throw "An executable must be specified"),
-             [string]$arguments,
-             [switch]$hidden,
-             [switch]$waitforexit,
-             [switch]$redirectStdOut
-             )   
-     
-     # Build Startinfo and set options according to parameters
-     $startinfo = new-object System.Diagnostics.ProcessStartInfo
-     $startinfo.FileName = $exe
-     $startinfo.Arguments = $arguments
-     if ($hidden){
-         $startinfo.WindowStyle = "Hidden"
-         $startinfo.CreateNoWindow = $true
-     }
-     if($redirectStdOut){
-        $startinfo.RedirectStandardOutput=$true
-        $startinfo.UseShellExecute=$false
-     }
-     $process = [System.Diagnostics.Process]::Start($startinfo)
-     if ($waitforexit) {$process.WaitForExit()}
-     $process 
-}
-	
-$argfile = 'exifargfile5'
-
-if (Test-Path $argfile) { del $argfile }           # delete any existing argfile
-$null | Out-File $argfile -Append -Encoding Ascii;    # and create an empty new one
-
-$p = start-proc "G:\exiftool.exe" "-stay_open True -@ $argfile" -redirectStdOut -hidden      #start exiftool, make it resident monitoring exifargfile, capturing stdout
-
-	
-	
 # Get the files which should be moved, without folders
-$files = Get-ChildItem 'G:\To Sort\2017 Test\2017-09-30' -Recurse | where {!$_.PsIsContainer}
-	 
+$files = Get-ChildItem 'G:\To Sort\2017 Test\' -Recurse | where {!$_.PsIsContainer}
+ 
 # List Files which will be moved
 #$files
-	 
+ 
 # Target Filder where files should be moved to. The script will automatically create a folder for the year and month.
 $targetPath = 'G:\Sorted\2017'
-	 
+ 
 foreach ($file in $files) {
 
-    # send exiftool the command to execute
-    "-datetimeoriginal"| Out-File $argfile -Append -Encoding Ascii;         # return me the filename of the processed file
-    "$file"               | Out-File $argfile -Append -Encoding Ascii;         # file to process
-    "-execute`n"       | Out-File $argfile -Append -Encoding Ascii;         # execute it
+    Write-Host "-----"
 
-    # read exiftool response from stdout 
-    # Readline() will wait for input if there is none (yet), effectively pausing script execution, giving exiftool time to process the command
-    $resultFilename = $p.StandardOutput.Readline()      # this returns a string in the form : "Filename        : somefilename"
-    $dummy          = $p.StandardOutput.Readline()      # read exiftool's termination string "{ready}"
+    $file.FullName
 
-    $resultFilename
-    $dummy
+    $file | Format-List
+   
+    $pinfo = New-Object System.Diagnostics.ProcessStartInfo
+    $pinfo.FileName = "G:\exiftool.exe"
+    $pinfo.RedirectStandardError = $true
+    $pinfo.RedirectStandardOutput = $true
+    $pinfo.UseShellExecute = $false
+    $pinfo.WindowStyle = "Hidden"
+    $pinfo.CreateNoWindow = $true
     
+    if ($file.Extension -eq ".JPG" -or $file.Extension -eq ".JPEG" -or $file.Extension -eq ".HEIC") {
+        $dateField = "DateTimeOriginal"
+    } elseif ($file.Extension -eq ".PNG") {
+        $dateField = "DateCreated"
+    } elseif ($file.Extension -eq ".MOV" -or $file.Extension -eq ".mp4") {
+        $dateField = "TrackCreateDate"
+    } elseif ($file.Extension -eq ".GIF") {
+        $dateField = "FileModifyDate"
+    } else {
+        $dateField = "TrackCreateDate"
+    }
 
-    $year = $DateTime.Year
-    $month = $DateTime.Month
-    $day = $DateTime.Day
+
+    $pinfo.Arguments = "-" + $dateField + ' "' + $file.FullName +'"'
+    $p = New-Object System.Diagnostics.Process
+    $p.StartInfo = $pinfo
+    $p.Start() | Out-Null
+    $p.WaitForExit()
+    $stdout = $p.StandardOutput.ReadToEnd()
+    $stderr = $p.StandardError.ReadToEnd()
+    Write-Host "stdout: $stdout"
+    Write-Host "stderr: $stderr"
+    $pos = $stdout.IndexOf(":")
+    $dateString = $stdout.Substring($pos+2).trim()
+    $dateString
+    $date = [datetime]::ParseExact($dateString,'yyyy:MM:dd HH:mm:ss',$null)
+    $date
+ 
+
+    $year = $date.Year
+    $month = $date.Month
+    $day = $date.Day
+
+    if ($month -lt 10) {
+	    $month = '0'+$month
+	    $month = $month.ToString()
+	}
+	
+	if ($day -lt 10) {
+	    $day = '0'+$day
+	    $day = $day.ToString()
+	}
+
 
     # Out FileName, year and month
-    $file.Name + ': ' + $year + "-" + $month + "-" + $day
+    $file.FullName + ': ' + $year + "-" + $month + "-" + $day
 
  
     # Set Directory Path
