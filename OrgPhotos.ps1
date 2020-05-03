09d53f99a2e15c020d329c9ee85bef2e678a87b9
Michael Plasmeier <2670107+theplaz@users.noreply.github.com>
2020-05-03 12:44:33 -0700

    Update OrgPhotos.ps1
    Try to use exif tools (not working)

--- OrgPhotos.ps1
+++ OrgPhotos.ps1
@@ -1,3 +1,38 @@
+function Start-Proc  {
+     param (
+             [string]$exe = $(Throw "An executable must be specified"),
+             [string]$arguments,
+             [switch]$hidden,
+             [switch]$waitforexit,
+             [switch]$redirectStdOut
+             )   
+     
+     # Build Startinfo and set options according to parameters
+     $startinfo = new-object System.Diagnostics.ProcessStartInfo
+     $startinfo.FileName = $exe
+     $startinfo.Arguments = $arguments
+     if ($hidden){
+         $startinfo.WindowStyle = "Hidden"
+         $startinfo.CreateNoWindow = $true
+     }
+     if($redirectStdOut){
+        $startinfo.RedirectStandardOutput=$true
+        $startinfo.UseShellExecute=$false
+     }
+     $process = [System.Diagnostics.Process]::Start($startinfo)
+     if ($waitforexit) {$process.WaitForExit()}
+     $process 
+}
+	
+$argfile = 'exifargfile5'
+
+if (Test-Path $argfile) { del $argfile }           # delete any existing argfile
+$null | Out-File $argfile -Append -Encoding Ascii;    # and create an empty new one
+
+$p = start-proc "G:\exiftool.exe" "-stay_open True -@ $argfile" -redirectStdOut -hidden      #start exiftool, make it resident monitoring exifargfile, capturing stdout
+
+	
+	
 # Get the files which should be moved, without folders
 $files = Get-ChildItem 'G:\To Sort\2017 Test\2017-09-30' -Recurse | where {!$_.PsIsContainer}
 	 
@@ -9,10 +44,23 @@ $targetPath = 'G:\Sorted\2017'
 	 
 foreach ($file in $files) {
 
-    $year = $file.LastWriteTime.Year.ToString()
-    $month = $file.LastWriteTime.Month.ToString()
-    $day = $DateTime.Day.ToString()
+    # send exiftool the command to execute
+    "-datetimeoriginal"| Out-File $argfile -Append -Encoding Ascii;         # return me the filename of the processed file
+    "$file"               | Out-File $argfile -Append -Encoding Ascii;         # file to process
+    "-execute`n"       | Out-File $argfile -Append -Encoding Ascii;         # execute it
 
+    # read exiftool response from stdout 
+    # Readline() will wait for input if there is none (yet), effectively pausing script execution, giving exiftool time to process the command
+    $resultFilename = $p.StandardOutput.Readline()      # this returns a string in the form : "Filename        : somefilename"
+    $dummy          = $p.StandardOutput.Readline()      # read exiftool's termination string "{ready}"
+
+    $resultFilename
+    $dummy
+    
+
+    $year = $DateTime.Year
+    $month = $DateTime.Month
+    $day = $DateTime.Day
 
     # Out FileName, year and month
     $file.Name + ': ' + $year + "-" + $month + "-" + $day
