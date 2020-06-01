17168be3d64c67e86de2e6c8c25dbe8bca8a7d2b
Michael Plasmeier <2670107+theplaz@users.noreply.github.com>
2020-05-31 18:43:38 -0700

    Get paths working for rename

--- RenameOnly.ps1
+++ RenameOnly.ps1
@@ -1,31 +1,35 @@
 ﻿# Get the files which should be moved, without folders
-$files = Get-ChildItem 'D:\Sorted\Output3' -Recurse | where {!$_.PsIsContainer} 
+$files = Get-ChildItem 'D:\Sorted\Output3' -Recurse | where {!$_.PsIsContainer}
  
 
 foreach ($file in $files) {
 
     Write-Host "-----"
-    $file
+    $file.FullName
+    $file.Basename
+    $file.Extension
+    $filePath = Split-Path -Path $file.FullName
+    $filePath
 
-    Write-Output ($file -like '*(Edited)*')
-    if ($file -like '*(Edited)*') {
+
+    if ($file.Basename -like '*(Edited)*') {
         Write-Output "leave name since Edited"
-        $newFileName = $file
+        $newFileName = $file.Basename
     } else {
        Write-Output "strip ()"
-       $newFileName = $file -replace '\([^\)]+\)'
+       $newFileName = $file.Basename -replace '\([^\)]+\)'
        $newFileName
     }
     Write-Output $newFileName
-    $newPathNewName = $Directory + "\" + $newFileName
+    $newPathNewName = $filePath + "\" + $newFileName + $file.Extension
     Write-Output $newPathNewName
 
     #if already exists
-    if (Test-Path $newPathCurrentName) {
-        Write-Output "file exists"
+    if (Test-Path $newPathNewName) {
+        Write-Output "file exists; skip"
     } else {
         Write-Output "rename"
-        #Rename-Item -Path $file -NewName $newFileNameRenameOnly
+        #Rename-Item -Path $file.FullName -NewName $newPathNewName
     }
 
 }
